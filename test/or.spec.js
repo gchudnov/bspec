47e21b1ce87f65bb31bb122fbf9192b865707e17
Grigoriy Chudnov <g.chudnov@gmail.com>
2015-02-07 16:52:32 +0300

    added OR test

---
+++ test/or.spec.js
@@ -0,0 +1,81 @@
+'use strict';
+
+var should = require('should');
+var util = require('util');
+var Spec = require('./../lib/bspec').Spec;
+
+describe('OR Specification', function() {
+
+  // TRUE
+  function TrueSpec() {
+  }
+
+  util.inherits(TrueSpec, Spec);
+
+  TrueSpec.prototype.isSatisfiedBy = function(dummy, cb) {
+    return cb(null, true);
+  };
+
+  // FALSE
+  function FalseSpec() {
+  }
+
+  util.inherits(FalseSpec, Spec);
+
+  FalseSpec.prototype.isSatisfiedBy = function(dummy, cb) {
+    return cb(null, false);
+  };
+
+  var alwaysTrue = new TrueSpec();
+  var alwaysFalse = new FalseSpec();
+
+
+  it('can be validated for true-true', function(done) {
+    alwaysTrue.or(alwaysTrue).isSatisfiedBy({}, function(err, flag) {
+      should.not.exist(err);
+      flag.should.be.true;
+      done();
+    });
+  });
+
+  it('can be validated for true-false', function(done) {
+    alwaysTrue.or(alwaysFalse).isSatisfiedBy({}, function(err, flag) {
+      should.not.exist(err);
+      flag.should.be.true;
+      done();
+    });
+  });
+
+  it('can be validated for false-true', function(done) {
+    alwaysFalse.or(alwaysTrue).isSatisfiedBy({}, function(err, flag) {
+      should.not.exist(err);
+      flag.should.be.true;
+      done();
+    });
+  });
+
+  it('can be validated for false-false', function(done) {
+    alwaysFalse.or(alwaysFalse).isSatisfiedBy({}, function(err, flag) {
+      should.not.exist(err);
+      flag.should.be.false;
+      done();
+    });
+  });
+
+  it('can be validated for true-true-true', function(done) {
+    alwaysTrue.or(alwaysTrue).or(alwaysTrue).isSatisfiedBy({}, function(err, flag) {
+      should.not.exist(err);
+      flag.should.be.true;
+      done();
+    });
+  });
+
+  it('can be validated for true-true-false', function(done) {
+    alwaysTrue.or(alwaysTrue).or(alwaysFalse).isSatisfiedBy({}, function(err, flag) {
+      should.not.exist(err);
+      flag.should.be.true;
+      done();
+    });
+  });
+
+});
