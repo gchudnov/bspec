88dad4361609b6591e387a39216ec05a818ba3c2
Grigoriy Chudnov <g.chudnov@gmail.com>
2015-02-08 00:54:33 +0300

    added tests for incomplete specs

--- test/incomplete.spec.js
+++ test/incomplete.spec.js
@@ -1,2 +1,53 @@
 'use strict';
 
+var should = require('should');
+var util = require('util');
+var bspec = require('./../lib/bspec');
+
+var Spec = bspec.Spec;
+var SyncSpec = bspec.SyncSpec;
+
+describe('Incomplete', function() {
+
+  describe('Async specification', function() {
+
+    it('cannot be used', function(done) {
+
+      function OverDueSpec() {
+      }
+      util.inherits(OverDueSpec, Spec);
+
+      // NOTE: forgot to implement isSatisfiedBy
+
+      var overDue = new OverDueSpec();
+      overDue.isSatisfiedBy({}, function(err) {
+        should.exist(err);
+        done();
+      });
+
+    });
+
+  });
+
+  describe('Sync specification', function() {
+
+    it('cannot be used', function(done) {
+
+      function OverDueSpec() {
+      }
+      util.inherits(OverDueSpec, SyncSpec);
+
+      // NOTE: forgot to implement isSatisfiedBy
+
+      var overDue = new OverDueSpec();
+      (function() {
+        overDue.isSatisfiedBy({});
+      }).should.throw();
+
+      done();
+
+    });
+
+  });
+
+});
