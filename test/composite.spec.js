25d279593b0e09e7c91e7d61456bfd157474948a
Grigoriy Chudnov <g.chudnov@gmail.com>
2015-02-07 23:47:21 +0300

    fixed test structure

--- test/composite.spec.js
+++ test/composite.spec.js
@@ -4,42 +4,38 @@ var should = require('should');
 var util = require('util');
 var Spec = require('./../lib/bspec').Spec;
 
-describe('Async', function() {
+describe('Composite Specification', function() {
 
-  describe('Composite Specification', function() {
+  // TRUE
+  function TrueSpec() {
+  }
 
-    // TRUE
-    function TrueSpec() {
-    }
+  util.inherits(TrueSpec, Spec);
 
-    util.inherits(TrueSpec, Spec);
+  TrueSpec.prototype.isSatisfiedBy = function(dummy, cb) {
+    return cb(null, true);
+  };
 
-    TrueSpec.prototype.isSatisfiedBy = function(dummy, cb) {
-      return cb(null, true);
-    };
+  // FALSE
+  function FalseSpec() {
+  }
 
-    // FALSE
-    function FalseSpec() {
-    }
+  util.inherits(FalseSpec, Spec);
 
-    util.inherits(FalseSpec, Spec);
+  FalseSpec.prototype.isSatisfiedBy = function(dummy, cb) {
+    return cb(null, false);
+  };
 
-    FalseSpec.prototype.isSatisfiedBy = function(dummy, cb) {
-      return cb(null, false);
-    };
-
-    var alwaysTrue = new TrueSpec();
-    var alwaysFalse = new FalseSpec();
+  var alwaysTrue = new TrueSpec();
+  var alwaysFalse = new FalseSpec();
 
 
-    it('can be validated for (true && false) || true', function(done) {
-      alwaysTrue.and(alwaysFalse).or(alwaysTrue).isSatisfiedBy({}, function(err, flag) {
-        should.not.exist(err);
-        flag.should.be.true;
-        done();
-      });
+  it('can be validated for an expression', function(done) {
+    alwaysTrue.and(alwaysFalse).or(alwaysTrue).isSatisfiedBy({}, function(err, flag) {
+      should.not.exist(err);
+      flag.should.be.true;
+      done();
     });
-
   });
 
 });
