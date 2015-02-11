ffa9a05506a473e6730100e252cc6e39244d1b47
Grigoriy Chudnov <g.chudnov@gmail.com>
2015-02-11 18:58:28 +0300

    added test for functions with no name property

---
+++ test/no-func-name.spec.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var should = require('should');
+var util = require('./../lib/util');
+
+
+describe('Function with no name property', function() {
+
+  it('can be traced', function(done) {
+
+    var f = {
+      toString: function() { return 'function myFunc(){}'; }
+    };
+
+    var s = util.functionName(f);
+    s.should.be.a.String.and.not.empty;
+
+    done();
+  });
+
+});
