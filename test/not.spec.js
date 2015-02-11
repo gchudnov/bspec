369f96694045f525c17fb6c7b351d872058938d4
Grigoriy Chudnov <g.chudnov@gmail.com>
2015-02-11 18:43:50 +0300

    added tests for error conditions

--- test/not.spec.js
+++ test/not.spec.js
@@ -31,8 +31,19 @@ describe('NOT Specification', function() {
       return cb(null, false);
     };
 
+    // ERROR
+    function ErrorSpec() {
+    }
+
+    util.inherits(ErrorSpec, Spec);
+
+    ErrorSpec.prototype.isSatisfiedBy = function(dummy, cb) {
+      return cb(new Error('cannot satisfy the spec'));
+    };
+
     var alwaysTrue = new TrueSpec();
     var alwaysFalse = new FalseSpec();
+    var alwaysError = new ErrorSpec();
 
 
     it('can be validated for !true', function(done) {
@@ -47,6 +58,13 @@ describe('NOT Specification', function() {
       alwaysFalse.not().isSatisfiedBy({}, function(err, flag) {
         should.not.exist(err);
         flag.should.be.true;
+        done();
+      });
+    });
+
+    it('should return an error for error', function(done) {
+      alwaysError.not().isSatisfiedBy({}, function(err) {
+        should.exist(err);
         done();
       });
     });
