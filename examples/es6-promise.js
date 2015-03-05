1680396d0f89b78a9570a3589e51cadf8dae646c
Grigoriy Chudnov <g.chudnov@gmail.com>
2015-03-05 22:28:23 +0300

    added promise spec examples

---
+++ examples/es6-promise.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * Specifications based on ES6 promises (no polyfill, Promise support is needed, e.g. io.js)
+ */
+
+var Spec = require('./../lib/bspec').PromiseSpec;
+
+function isRunning(operation) {
+  return Promise.resolve(operation.status === 'running');
+}
+
+function isPending(operation) {
+  return Promise.resolve(operation.status === 'pending');
+}
+
+var executableSpec = Spec(isRunning).or(isPending);
+
+var operation1 = { status: 'running' };
+var operation2 = { status: 'stopped' };
+
+executableSpec.isSatisfiedBy(operation1)
+  .then(function(flag) {
+    console.log('is operation1 executable: ', flag);
+  });
+
+executableSpec.isSatisfiedBy(operation2)
+  .then(function(flag) {
+    console.log('is operation2 executable: ', flag);
+  });
+
